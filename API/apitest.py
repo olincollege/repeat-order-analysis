f0cffd260c7618cd87994184b005de3f8354918e
Andrew Kurtz <akurtz@olin.edu>
2024-03-10 15:15:53 -0400

    Added test and saving

--- API/apitest.py
+++ API/apitest.py
@@ -1,7 +1,9 @@
 import etsy_api
 from json_saving import save_to_json, read_from_json, save_output_to_json
+from requests_oauthlib import OAuth2Session
 
 access_token, refresh_token, date = read_from_json()
+
 
 api = etsy_api.EtsyAPI(
     "xm01cxk1qnki67tzfe77r61m", access_token, refresh_token, date, save_to_json
