d849586e34461678f40c3d58aa1672a16ed4df46
Andrew Kurtz <akurtz@olin.edu>
2024-03-11 19:08:48 -0400

    Updated analyze_data with correct paths

--- analyze_data.py
+++ analyze_data.py
@@ -4,8 +4,8 @@ Analyze repeat customer order data from an Etsy shop
 
 from api_lib import get_all_orders
 
-KEY_PATH = "/home/akurtz/repeat-order-analysis/API/keys.json"
-DATA_PATH = "/home/akurtz/repeat-order-analysis/API/orders.json"
+KEY_PATH = "/home/akurtz/repeat-order-analysis/keys.json"
+DATA_PATH = "/home/akurtz/repeat-order-analysis/orders.json"
 SHOP_ID = 23574688
 
 get_all_orders(KEY_PATH, DATA_PATH, SHOP_ID)
